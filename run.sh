1f480aa005591dc6e7a2f23c6c9a4c4623660982
Hector9000@RaspberryPi <info@esotronic.de>
2019-11-30 21:21:43 +0100

    run script updated

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+tmux new-session -d -s hector 'python3 src/HardwareRunner.py'
+tmux new-window -t  hector:1 'python3 src/NeoPixel.py'
+#python3 src/main.py
+
 cd src/ && KIVY_GL_BACKEND=gl python3 main.py
