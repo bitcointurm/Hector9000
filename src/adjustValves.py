c9360bbfd58d343e5f078b8d51d33d3746fc1a9a
Simon Honc <simon.honc@groupm.com>
2019-12-08 23:21:43 +0100

    addjust finger added for value >23

--- src/adjustValves.py
+++ src/adjustValves.py
@@ -10,11 +10,15 @@ while True:
     vnum = int(input("Bitte Ventilnr. eingeben (0..23); Ende mit -1:  "))
     if vnum == -1:
         sys.exit()
-    cpos = h.valvePositions[vnum][1]
+    cpos = h.fingerPosition[1]
+    if vnum < 24:
+        cpos = h.valvePositions[vnum][1]
     print("Ventil %d wird geschlossen, Servoposition = %d" % (vnum, cpos))
     while cpos != -1:
         if vnum < 12:
             h.pca.set_pwm(vnum, 0, cpos)
+        elif vnum >= 24:
+            h.pca.set_pwm(13, 0, cpos)
         else:
             h.pcaplus.set_pwm(vnum-12, 0, cpos)
         cpos = int(input("Bitte neue Servoposition eingeben:"))
