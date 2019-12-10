14d2bf0a9bf3ccf46b31e9813492f88301ffecd1
Hector9000@RaspberryPi <info@esotronic.de>
2019-12-10 21:59:41 +0100

    logging added

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-tmux new-session -d -s hector 'python3 src/HardwareRunner.py'
-tmux new-window -t  hector:1 'python3 src/NeoPixel.py'
+#tmux new-session -d -s hector 'python3 src/HardwareRunner.py'
+#tmux new-window -t  hector:1 'python3 src/NeoPixel.py'
 #python3 src/main.py
 
-cd src/ && KIVY_GL_BACKEND=gl python3 main.py
+cd /home/pi/Satoshi24/src
+KIVY_GL_BACKEND=gl /usr/bin/python3 main.py &> last-run.log 2>&1
