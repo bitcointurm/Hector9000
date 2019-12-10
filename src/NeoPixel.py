6a6325d92bb171bfbf4f94b54f811dfc8c6019a4
Hector9000@RaspberryPi <info@esotronic.de>
2019-12-10 22:01:49 +0100

    neo start config

--- src/NeoPixel.py
+++ src/NeoPixel.py
@@ -3,7 +3,7 @@ import board
 import neopixel
 
 PORT = board.D18
-NUM = 15
+NUM = 12
 NUMBASE = 5
 
 pixels = neopixel.NeoPixel(PORT, NUM)
@@ -63,7 +63,7 @@ num_pixels = NUM
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
+ORDER = neopixel.RGB
 
 
 # pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
