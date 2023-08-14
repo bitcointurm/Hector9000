1d2a14c3327f3e5b293618fc6347433123e81235
bitcointurm@protonmail.com <bitcointurm@protonmail.com>
2023-08-14 22:20:50 +0200

    use bconf drinks again

--- src/drinks.py
+++ src/drinks.py
@@ -2,6 +2,18 @@
 
 drink_list = [
     {
+        "name": "Bitcoin im Turm",
+        "sound": "music/SatoshiCountry.mp3",
+        "image": "img/bitcoinimturm.png",
+        "recipe": [
+            ("coconut", 6),
+            ("zitrone", 25),
+            ("annanas", 52),
+            ("brum", 52),
+            ("kahlua", 13),
+            ("orange", 52)
+        ]
+    }, {
         "name": "Nasty Ass Honey Badger",
         "sound": "music/Honeybadger.mp3",
         "image": "img/honeybadger.jpg",
@@ -11,6 +23,17 @@ drink_list = [
             ("coconut", 20),
             ("lime", 10),
             ("cola", 120),
+        ]
+    }, {
+        "name": "Sats on the beach",
+        "sound": "music/PlebRap.mp3",
+        "image": "img/PlebRap.jpg",
+        "recipe": [
+            ("vodka", 35),
+            ("orange", 68),
+            ("apricot", 17),
+            ("cranberry", 68),
+            ("zuckersirup", 12)
         ]
     }, {
         "name": "JodlHodler",
@@ -55,6 +78,17 @@ drink_list = [
             ("lime", 21),
         ]
     }, {
+        "name": "Bitcoin Runners Cocktail",
+        "image": "img/Runner.png",
+        "sound": "music/runningman.mp3",
+        "recipe": [
+            ("coconut", 9),
+            ("zitrone", 18),
+            ("annanas", 72),
+            ("brum", 46),
+            ("marakuja", 55)
+        ]
+    }, {
         "name": "Bitcoin Chicks on the Blocks",
         "image": "img/BtcChicks.jpg",
         "sound": "music/girlsfun.mp3",
@@ -72,6 +106,17 @@ drink_list = [
             ("vodka", 46),
             ("lime", 18),
             ("ginger", 136)
+        ]
+    }, {
+        "name": "Posch Punch",
+        "image": "img/PoschPunch.png",
+        "sound": "music/antonop.mp3",
+        "recipe": [
+            ("rum", 24),
+            ("grenadine", 12),
+            ("lime", 24),
+            ("brum", 46),
+            ("orange", 94)
         ]
     }, {
         "name": "Fomo",
@@ -93,6 +138,17 @@ drink_list = [
             ("ginger", 118)
         ]
     }, {
+        "name": "Touch Down",
+        "image": "img/TouchDown.png",
+        "sound": "music/iota.mp3",
+        "recipe": [
+            ("grenadine", 12),
+            ("zitrone", 24),
+            ("vodka", 46),
+            ("apricot", 24),
+            ("marakuja", 94)
+        ]
+    }, {
         "name": "Bavarian Badger",
         "image": "img/badger.jpg",
         "sound": "music/HodlerSong.mp3",
@@ -101,6 +157,26 @@ drink_list = [
             ("grenadine", 20),
             ("tonic", 100),
             ("lime", 20)
+        ]
+    }, {
+        "name": "Lina Colada",
+        "image": "img/LinaColada.png",
+        "sound": "music/StackingSats.mp3",
+        "recipe": [
+            ("rum", 25),
+            ("coconut", 25),
+            ("annanas", 100),
+            ("brum", 25),
+            ("sahne", 25)
+        ]
+    }, {
+        "name": "Bitcoin Plunge",
+        "image": "img/BtcPlunge.png",
+        "sound": "music/fomo.mp3",
+        "recipe": [
+            ("vodka", 34),
+            ("kahlua", 33),
+            ("sahne", 33)
         ]
     }, {
         "name": "Hal Ginny",
@@ -112,6 +188,17 @@ drink_list = [
             ("cola", 80)
         ]
     }, {
+        "name": "Mem Pool",
+        "sound": "music/PlebRap.mp3",
+        "image": "img/Mempool.png",
+        "recipe": [
+            ("vodka", 42),
+            ("coconut", 21),
+            ("annanas", 105),
+            ("sahne", 21),
+            ("blue", 11)
+        ]
+    },{
         "name": "Vodka Livera",
         "image": "img/livera.png",
         "sound": "music/HodlerSong.mp3",
@@ -164,6 +251,23 @@ drink_list = [
             ("ginger", 136)
         ]
     }, {
+        "name": "It's a Bubble",
+        "image": "img/BtcBubble.png",
+        "sound": "music/fomo.mp3",
+        "recipe": [
+            ("whisky", 27),
+            ("cola", 173)   
+        ]
+    }, {
+        "name": "Stacking Sats",
+        "image": "img/TFTC.png",
+        "sound": "music/StackingSats.mp3",
+        "recipe": [
+            ("vodka", 40),
+            ("lime", 10),
+            ("soda", 150)
+        ]
+    }, {
         "name": "Lightning Lemonade",
         "image": "img/LightningLimo.jpg",
         "sound": "music/PlebRap.mp3",
@@ -185,6 +289,19 @@ drink_list = [
             ("ginger", 136)
         ]
     }, {
+        "name": "7 tall black women",
+        "image": "img/nodl.jpg",
+        "sound": "music/giacomo.mp3",
+        "recipe": [
+            ("rum", 10),
+            ("gin", 20),
+            ("vodka", 10),
+            ("coconut", 20),
+            ("tonic", 20),
+            ("lime", 10),
+            ("cola", 40)
+        ]
+    }, {
         "name": "Matt Colado",
         "image": "img/bluematt.png",
         "sound": "music/SatoshiCountry.mp3",
@@ -200,6 +317,25 @@ drink_list = [
         "recipe": [
             ("whisky", 40),
             ("lime", 60)
+        ]
+    }, {
+        "name": "Virgin Colada",
+        "image": "img/SatsOnTheBeach.png",
+        "sound": "music/WhatsBtc.mp3",
+        "recipe": [
+            ("coconut", 25),
+            ("annanas", 150),
+            ("sahne", 25)
+        ]
+    }, {
+        "name": "Sh**tcoin's Punch",
+        "image": "img/vob.png",
+        "sound": "music/Umfrage.mp3",
+        "recipe": [
+            ("rum", 40),
+            ("grenadine", 10),
+            ("annanas", 80),
+            ("lime", 10)
         ]
     }, {
         "name": "iota",
